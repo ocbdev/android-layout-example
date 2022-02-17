049ba614d64b9420707236b74c8eb254ee4e1683
Onur Bozkurt <onur.bozkurt@talhanakliyat.com.tr>
2022-02-17 09:17:14 +0300

    upload

---
+++ app/src/main/java/com/examples/java001/SecondFragment.java
@@ -0,0 +1,32 @@
+package com.examples.java001;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+
+
+public class SecondFragment extends Fragment {
+
+
+
+
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+    }
+
+}
